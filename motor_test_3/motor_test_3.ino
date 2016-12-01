8d7daa3aa8906504683a5f6fd86801d7272369a4
Paul Krusell <paul.krusell@students.olin.edu>
2016-12-01 16:12:50 -0500

    updated to work i think. maybe some timing issues with updating motor speed tho

--- motor_test_3/motor_test_3.ino
+++ motor_test_3/motor_test_3.ino
@@ -1,35 +1,37 @@
 #include <Servo.h>
 
-const int turn_motor_pin = 3;
-const int thrust_motor_pin = 2;
-const int STOP = 1500;
-const int MIN = 1250;
-const int MAX = 1750;
+const int MOTOR_L_pin = 2;
+const int MOTOR_R_pin = 3;
+const int STOP = 1570;
+const int MIN = 1300;
+const int MAX = 1700;
+int timer = 0;
 
-int SPEED = STOP;
-int SPEED_NOW = STOP;
+int SPEED_L[6] = {STOP, MAX,  MAX,  MAX, 1625, 1550};
+int SPEED_R[6] = {STOP, 1550, 1625, MAX, MAX,  MAX};
+
+int MODE = 0;
+int SPEED_NOW_L = STOP;
+int SPEED_NOW_R = STOP;
 const int DELTA_SPEED = 5;
 
 int eStopPin = 7;
 unsigned long time1;
 unsigned long time2;
 
-Servo turn_motor;   // controls the turning part of the roboclaw
-Servo thrust_motor; // controls the speed part of the roboclaw
+Servo MOTOR_L;   
+Servo MOTOR_R; 
 
 void setup() {
   Serial.begin(9600);
-  pinMode(turn_motor_pin, OUTPUT);
-  pinMode(thrust_motor_pin, OUTPUT);
+  pinMode(MOTOR_L_pin, OUTPUT);
+  pinMode(MOTOR_R_pin, OUTPUT);
   pinMode(eStopPin, INPUT);
-  turn_motor.attach(turn_motor_pin);
-  thrust_motor.attach(thrust_motor_pin);
+  MOTOR_L.attach(MOTOR_L_pin);
+  MOTOR_R.attach(MOTOR_R_pin);
 
-  thrust_motor.writeMicroseconds(SPEED);
-  //  pinMode(e/stop, INPUT)
-  //  while (!Serial);
-  //  Serial.println("speed 1k to 2k, 1.5k neutral");
-  // put your setup code here, to run once:
+  MOTOR_L.writeMicroseconds(SPEED_L[0]);
+  MOTOR_R.writeMicroseconds(SPEED_R[0]);
 
 }
 
@@ -38,41 +40,43 @@ void loop() {
   if (Serial.available())
   {
     time1 = millis();
-    SPEED = Serial.parseInt();
-    Serial.println(SPEED);
+    MODE = Serial.parseInt();
+    Serial.println(MODE);
   }
-
-
-  if (SPEED >= 1000 && SPEED <= 2000)
-  {
-    if (SPEED_NOW < SPEED) {
-      SPEED_NOW += DELTA_SPEED;
-      thrust_motor.writeMicroseconds(SPEED_NOW);
-      delay(1000);
-      Serial.println(SPEED_NOW);
-    }
-    if (SPEED_NOW > SPEED) {
-      SPEED_NOW -= DELTA_SPEED;
-      thrust_motor.writeMicroseconds(SPEED_NOW);
-      delay(1000);
-      Serial.println(SPEED_NOW);
-    }
-  }
-
-
+if (SPEED_NOW_L != SPEED_L[MODE]){
+SPEED_NOW_L =  ramp_speed(SPEED_NOW_L, SPEED_L[MODE], MOTOR_L);
+}
+if (SPEED_NOW_R != SPEED_L[MODE]){
+SPEED_NOW_R =  ramp_speed(SPEED_NOW_R, SPEED_R[MODE], MOTOR_R);
+}
   if (readEstop() == 0)
     {
-      SPEED = 1500;
-      SPEED_NOW = 1500;
+      MODE = 0;
+//      SPEED_NOW = 1500;
       Serial.println("estop pressed");
     }
-
-  //turn_motor_pin.writeMicroseconds(STOP); // this stops
-  //thrust_motor.writeMicroseconds(STOP);   // stops
 }
 
 boolean readEstop() {
   boolean eStopTriggered = digitalRead(eStopPin);
   return eStopTriggered;
+}
+
+int ramp_speed(int SPEED_NOW,int  SPEED, Servo MOTOR)
+{
+//  int SPEED_NOW;
+  if (SPEED_NOW < SPEED) {
+      SPEED_NOW += DELTA_SPEED;
+      MOTOR.writeMicroseconds(SPEED_NOW);
+      delay(100);
+      Serial.println(SPEED_NOW);
+    }
+    if (SPEED_NOW > SPEED) {
+      SPEED_NOW -= DELTA_SPEED;
+      MOTOR.writeMicroseconds(SPEED_NOW);
+      delay(100);
+      Serial.println(SPEED_NOW);
+    }
+  return SPEED_NOW;
 }
 
